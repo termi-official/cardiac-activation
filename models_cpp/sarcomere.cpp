c5c98b8113ccdb4afee133b78df1d513a715c6b3
termi-official <termi-official@users.noreply.github.com>
2025-02-20 19:41:54 +0100

    Also output states

--- models_cpp/sarcomere.cpp
+++ models_cpp/sarcomere.cpp
@@ -62,6 +62,8 @@ sarcomere::solve(const std::function<double(const double &time)> &Ca,
         results["dSL_dt"].push_back(dSL_dt);
         results["Ta"].push_back(get_active_tension(state_at_t, sarcomere_length));
         results["As"].push_back(get_active_stiffness(state_at_t, sarcomere_length));
+        for(int i=0;i<20;i++)
+          results["S" + std::__cxx11::to_string(i)].push_back(state_at_t[i]);
 
         last_time_write += dt_write;
       }
@@ -84,12 +86,19 @@ void sarcomere::write_csv(std::map<std::string, std::vector<double>> results,
                           std::string file_name) {
   std::ofstream csvfile;
   csvfile.open(file_name);
-  csvfile << "t,Ca,SL,dSL_dt,Ta,As" << std::endl;
+  csvfile << "t,Ca,SL,dSL_dt,Ta,As";
+  for(int i=0;i<20;i++)
+          csvfile << ",S"+ std::__cxx11::to_string(i);
+  csvfile << std::endl;
   csvfile << std::scientific << std::setprecision(8);
-  for (unsigned int i = 0; i < results["time"].size(); ++i)
+  for (unsigned int i = 0; i < results["time"].size(); ++i) {
     csvfile << results["time"][i] << "," << results["Ca"][i] << ","
             << results["SL"][i] << "," << results["dSL_dt"][i] << ","
-            << results["Ta"][i] << "," << results["As"][i] <<  std::endl;
+            << results["Ta"][i] << "," << results["As"][i];
+    for(int i=0;i<20;i++)
+          csvfile << "," << results["S" + std::__cxx11::to_string(i)][i];
+    csvfile << std::endl;
+  }
 
   csvfile.close();
 }
